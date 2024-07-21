9b30fcc084fe61b5aa70a2b33b6c6bcfda081f13
Jonathan Kocur <kocurcola417@gmail.com>
2024-07-21 19:06:51 -0400

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -3,6 +3,7 @@
 
 int main() {
 
+  // Here is the main file where the object is accessed and the profile information is entered. Then a hobby is added and the profile is displayed
   Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
   sam.add_hobby("listening to audiobooks and podcasts");
 
