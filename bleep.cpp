bd8f16838a9704c0170bf5e6e3a7e31aee78f295
Jonathan Kocur <kocurcola417@gmail.com>
2024-07-22 13:13:26 -0400

    Update bleep.cpp

--- bleep.cpp
+++ bleep.cpp
@@ -5,17 +5,11 @@
 int main() {
 
   std::string word = "broccoli";
-
   std::string sentence = "Blah blah blah";
-
   bleep(word, sentence);
   
   for (int i = 0; i < sentence.size(); i++) {
-    
     std::cout << sentence[i];
-    
   }
-  
   std::cout << "\n";
-  
 }
