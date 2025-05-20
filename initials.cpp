a5796f886b76582d269f20999dcfcbe089a9aecb
Jonathan Kocur <kocurcola417@gmail.com>
2025-05-20 10:02:42 -0400

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+  // This is my program, it is cute and quirky
+  std::cout << "JJJJJ  K   K" << "\n";
+  std::cout << "  J    K   K" << "\n";
+  std::cout << "  J    K  K" << "\n";
+  std::cout << "  J    KKK  " << "\n";
+  std::cout << "J J    K  K" << "\n";
+  std::cout << "J J    K   K" << "\n";
+  std::cout << "JJJ    K   K" << "\n";
+}
